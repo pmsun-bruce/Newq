38d161d83fc7c665fbccf6f14405240c176575b1
urielvan <urielvan@gmail.com>
2016-03-02 22:01:50 +0800

    Update DbTable.cs
    Add comments and change a bit

--- src/Newq/DbTable.cs
+++ src/Newq/DbTable.cs
@@ -1,23 +1,23 @@
-﻿namespace Newq
+namespace Newq
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
-    /// 
+    /// Simulate tables in Sql
     /// </summary>
     public class DbTable
     {
         /// <summary>
-        /// 
+        /// An dictionary used to get columns in the table
         /// </summary>
         private Dictionary<string, DbColumn> columns;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DbTable"/> class.
+        /// Constructor
         /// </summary>
-        /// <param name="type"></param>
+        /// <param name="type">Type of the corresponding class</param>
         public DbTable(Type type)
         {
             Name = type.Name;
@@ -26,9 +26,9 @@
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DbTable"/> class.
+        /// Constructor
         /// </summary>
-        /// <param name="entity"></param>
+        /// <param name="entity">An entity of the corresponding class</param>
         public DbTable(object entity)
         {
             var type = entity.GetType();
@@ -39,21 +39,21 @@
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DbTable"/> class.
+        /// Constructor
         /// </summary>
-        /// <param name="name"></param>
+        /// <param name="name">Name of the corresponding class</param>
         internal DbTable(string name)
         {
             Name = name;
         }
 
         /// <summary>
-        /// 
+        /// Name of this
         /// </summary>
         public string Name { get; private set; }
 
         /// <summary>
-        /// 
+        /// Define which column in this should be the primary key.
         /// </summary>
         public DbColumn PrimaryKey
         {
@@ -69,7 +69,7 @@
         }
 
         /// <summary>
-        /// 
+        /// A read-only list contains the columns in this
         /// </summary>
         public IReadOnlyList<DbColumn> Columns
         {
@@ -77,10 +77,10 @@
         }
 
         /// <summary>
-        /// 
+        /// Get an certain entity of column by its name with an indexer
         /// </summary>
-        /// <param name="columnName"></param>
-        /// <returns></returns>
+        /// <param name="columnName">name of the column</param>
+        /// <returns>an entity of column</returns>
         public DbColumn this[string columnName]
         {
             get
@@ -95,9 +95,9 @@
         }
 
         /// <summary>
-        /// 
+        /// Insert all columns and set primary key if needed
         /// </summary>
-        /// <param name="type"></param>
+        /// <param name="type">Type of the corresponding class</param>
         private void SetColumns(Type type)
         {
             if (type == null)
@@ -105,36 +105,43 @@
                 throw new ArgumentNullException(nameof(type));
             }
 
+            // an entity used to be added to list
             DbColumn column = null;
+            // used to record name of each property
             var columnName = string.Empty;
-            var properties = type.GetProperties().Where(p => p.CanWrite);
-            var idProperty = properties.Where(p => p.Name == "Id");
+            // whether the corresponding class has property named Id
+            var hasId = false;
+            // only properties that can be set is needed(except "Id")
+            var properties = type.GetProperties().Where(p => {
+                if (p.Name == "Id")
+                {
+                    hasId = true;
 
-            if (idProperty.Count() > 0)
+                    return false;
+                }
+
+                return p.CanWrite;
+            });
+
+            if (hasId)
             {
-                columnName = "Id";
-                column = new DbColumn(this, columnName);
-                columns.Add(columnName, column);
+                column = new DbColumn(this, "Id");
+                columns.Add("Id", column);
             }
 
             foreach (var prop in properties)
             {
                 columnName = prop.Name;
-
-                if (columnName == "Id")
-                {
-                    continue;
-                }
-
                 column = new DbColumn(this, columnName);
                 columns.Add(columnName, column);
             }
         }
 
         /// <summary>
-        /// 
+        /// Insert all columns which has certain value
+        /// and set primary key if needed
         /// </summary>
-        /// <param name="entity"></param>
+        /// <param name="entity">An entity of the corresponding class</param>
         private void SetAssignedTable(object entity)
         {
             if (entity == null)
@@ -142,37 +149,46 @@
                 throw new ArgumentNullException(nameof(entity));
             }
 
+            // an entity used to be added to list
             DbColumn column = null;
-            object columnValue = null;
+            // used to record name of each property
             var columnName = string.Empty;
-            var properties = entity.GetType().GetProperties().Where(p => p.CanWrite);
-            var idProperty = properties.Where(p => p.Name == "Id");
+            // whether the corresponding class has property named Id
+            var hasId = false;
+            // used to record the value of each property
+            object columnValue = null;
+            // only properties that can be set is needed(except "Id")
+            var properties = entity.GetType().GetProperties().Where(p => {
+                if (p.Name == "Id")
+                {
+                    hasId = true;
+                    columnValue = p.GetValue(entity);
 
-            if (idProperty.Count() > 0)
+                    return false;
+                }
+
+                return p.CanWrite;
+            });
+
+            if (hasId)
             {
-                columnValue = idProperty.First().GetValue(entity);
-                columnName = "Id";
-                column = new DbColumn(this, columnName, columnValue);
-                columns.Add(columnName, column);
+                column = new DbColumn(this, "Id", columnValue);
+                columns.Add("Id", column);
             }
 
             foreach (var prop in properties)
             {
                 columnValue = prop.GetValue(entity);
                 columnName = prop.Name;
-
-                if (columnName != "Id")
-                {
-                    column = new DbColumn(this, columnName, columnValue);
-                    columns.Add(columnName, column);
-                }
+                column = new DbColumn(this, columnName, columnValue);
+                columns.Add(columnName, column);
             }
         }
 
         /// <summary>
-        /// 
+        /// Override ToString
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Name of this with a pair of square brackets</returns>
         public override string ToString()
         {
             return string.Format("[{0}]", Name);
